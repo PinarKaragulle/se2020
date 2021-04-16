8e7fdc42790a760973510f694ce5ebe46d16dd39
PinarKaragulle <72393604+PinarKaragulle@users.noreply.github.com>
2021-04-16 23:26:03 +0300

    implement endpoints of service

---
+++ se2020/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from se2020.morse import encode_morse
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	payload = {"result" : "Hello, world"}
+	return jsonify(payload)
+	
+	
+	
+
+@app.route("/morse/<phrase>")
+def encode(phrase):
+    payload = {"result": encode_morse(phrase)}
+    return jsonify(payload)
+
+
+if __name__ == "__main__":
+    app.run()
